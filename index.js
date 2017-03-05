69da03b83185af21bd98e7d84c9fe001f9d65f4c
rogerbf <roger@bergfalk.nu>
2017-03-05 21:47:53 +0100

    rename get-type

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const factory = require(`protostar`)
-const _typeof = require(`./get-type`)
+const _typeof = require(`./enhanced-typeof`)
 const { keys, assign } = Object
 
 const defaultConfiguration = {
@@ -21,45 +21,19 @@ const defaultConfiguration = {
 
 const parse = (factory, configuration, options) => {
   const { prefix, alias, behaviours } = configuration
-  const _parse = parse.bind(null, factory, configuration)
 
   return keys(options)
   .reduce((args, key) => {
     const value = options[key]
-
     // Convert aliases
     const option = keys(alias).includes(key) ? alias[key] : key
 
-    switch (_typeof(value)) {
-      case `string`:
-        return args.concat(
-          behaviours.string(_parse, prefix, option, value)
-        )
-      case `number`:
-        return args.concat(
-          behaviours.number(_parse, prefix, option, value)
-        )
-      case `array`:
-        return args.concat(
-          behaviours.array(_parse, prefix, option, value)
-        )
-      case `boolean`:
-        return args.concat(
-          behaviours.boolean(_parse, prefix, option, value)
-        )
-      case `undefined`:
-        return args.concat(
-          behaviours.undefined(_parse, prefix, option, value)
-        )
-      case `object`:
-        return args.concat(
-          behaviours.object(_parse, prefix, option, value)
-        )
-      case `null`:
-        return args.concat(
-            behaviours.null(_parse, prefix, option, value)
-        )
-    }
+    return args.concat(behaviours[_typeof(value)](
+      parse.bind(null, factory, configuration),
+      prefix,
+      option,
+      value
+    ))
   }, [])
 }
 
